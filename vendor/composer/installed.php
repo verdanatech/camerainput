c56a9672faa129465356c8781022a836bf795be0
verdanatech-marcio <marcio.fernandes@verdanatech.com>
2022-03-18 08:46:46 -0300

    Clone do Plugin

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v1.0.2',
+        'version' => '1.0.2.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9fd066c8c45895db2f4f9731b300d80a06e450b3',
+        'name' => '__root__',
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'v1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9fd066c8c45895db2f4f9731b300d80a06e450b3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
